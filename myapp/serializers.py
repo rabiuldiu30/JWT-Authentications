e3812bdce2c4a159dfdacbbb0c46f841c2b25378
Md. Rabiul Islam <120656583+rabiuldiu30@users.noreply.github.com>
2026-08-13 00:06:14 +0600

    Added RegisterSerializer

--- myapp/serializers.py
+++ myapp/serializers.py
@@ -5,3 +5,8 @@ class UserSerializer(serializers.ModelSerializer):
   class Meta:
     model = User
     fields = ['id', 'username', 'email']
+
+class RegisterSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = User
+    fields = ['username', 'email', 'password']
