18b830fdbb18dc7ee675a0aea08ea90c86626b48
Md. Rabiul Islam <120656583+rabiuldiu30@users.noreply.github.com>
2026-08-13 14:28:24 +0600

    Register api done

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from django.contrib.auth.models import User
+from .serializers import UserSerializer, RegisterSerializer, LoginSerializer
 
 # Create your views here.
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
